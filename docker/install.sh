f2c76242b5a13dc874b40603cc49090bb9281604
ltgoter <ltfwimn@gmail.com>
2020-11-03 10:28:34 +0800

    Create install.sh

---
+++ docker/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+
+
+install docker
+apt-get install -y \
+ apt-transport-https \
+ ca-certificates \
+ curl \
+ gnupg-agent \
+ software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+
+apt-cache madison docker-ce
+
+apt-get update
+apt-get install -y docker-ce=17.06.0~ce-0~ubuntu
